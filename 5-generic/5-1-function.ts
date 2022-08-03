dfbaa9009813132689a06af3945943f3f5a4f6c5
Guntak Kim <caputdraconis@kakao.com>
2022-08-03 18:18:23 +0900

    function generic

---
+++ 5-generic/5-1-function.ts
@@ -0,0 +1,17 @@
+{
+  function badCheckNotNull(arg: number | null): number {
+    if (arg == null) {
+      throw new Error("not valid number");
+    }
+    return arg;
+  }
+
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error("not valid number");
+    }
+    return arg;
+  }
+
+  const num = checkNotNull(23);
+}
